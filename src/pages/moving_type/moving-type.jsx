ab0dffa02eb30c318ef9b18644b59ecd28edecfb
salman <alysalman15@gmail.com>
2024-01-23 11:11:59 +0500

    make build

--- src/pages/moving_type/moving-type.jsx
+++ src/pages/moving_type/moving-type.jsx
@@ -21,7 +21,7 @@ function MovingType() {
                 <h2 className=" text-[#13C265] text-2xl font-semibold mb-4">Moving Type</h2>
             </div>
             <div className="felx items-center justify-between">
-                <button className=" w-40 bg-[#D1D1D1] flex-wrap m-2 p-1 rounded shadow text-md sm:text-base lg:text-lg">Local</button>
+                <button className=" w-40 bg-[#D1D1D1] flex-wrap m-2 p-1 rounded shadow text-md sm:text-base lg:text-lg active:text-red-blue">Local</button>
                 <button className=" w-40 bg-[#D1D1D1] flex-wrap m-2 p-1 rounded shadow text-md sm:text-base lg:text-lg">Commerical</button>
                 <button className=" w-40 bg-[#D1D1D1] flex-wrap m-2 p-1 rounded shadow text-md sm:text-base lg:text-lg">International</button>
             </div>
