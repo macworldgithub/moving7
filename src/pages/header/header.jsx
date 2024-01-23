16c81d1b34fd65009b9aecaae6dcb13fc70e1fa0
salman <alysalman15@gmail.com>
2024-01-23 17:14:15 +0500

    code push

--- src/pages/header/header.jsx
+++ src/pages/header/header.jsx
@@ -5,12 +5,12 @@ function Header() {
   return (
     <div className="header px-8 bg-transparent h-20 flex items-center justify-around gap-8 lg:gap-[20%]">
       <div className="logo">
-        <img src={logo} alt="logo"/>
+        <img src={logo} alt="logo" />
       </div>
       <div className="header-btn hidden lg:flex gap-3 lg:text-xl">
-        <button className="text-sm py-2 bg-[#13C265] lg:w-40 font-bold">Review</button>
-        <button className="text-sm py-2 bg-[#13C265] lg:w-40 font-bold">About Us</button>
-        <button className="text-sm py-2 bg-[#13C265] lg:w-40 font-bold">Become a partner</button>
+        <button className="text-sm py-2 bg-[#13C265] lg:text-lg lg:w-40 font-bold">Review</button>
+        <button className="text-sm py-2 bg-[#13C265] lg:text-lg lg:w-40 font-bold">About Us</button>
+        <button className="text-sm py-2 bg-[#13C265] lg:text-lg lg:w-40 font-bold">Become a partner</button>
       </div>
     </div>
   );
