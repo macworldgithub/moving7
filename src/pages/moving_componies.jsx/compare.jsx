93ed64868df1c8447ec824c4ca0087529e02decf
salman <alysalman15@gmail.com>
2024-01-23 17:36:14 +0500

    update

--- src/pages/moving_componies.jsx/compare.jsx
+++ src/pages/moving_componies.jsx/compare.jsx
@@ -9,7 +9,7 @@ function Compare() {
     <div>
       <div className="flex flex-col justify-center items-center pt-2">
         <div className="text-center pb-6 sm:pt-8">
-          <h2 className="text-2xl font-semibold">Compare moving companies like:</h2>
+          <h2 className="text-2xl font-semibold px-3">Compare moving companies like:</h2>
         </div>
         <div>
           <div className="flex-wrap p-4 hidden md:block">
