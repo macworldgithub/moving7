045e387b3cd6165a4573cf5d6729b7ac89fb5c93
salman <alysalman15@gmail.com>
2024-02-01 16:09:44 +0500

    form shadow

--- src/pages/partner/form/form.jsx
+++ src/pages/partner/form/form.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Form, Input, Button } from 'antd';
 
+import './form.css'
+
 const PartnerForm = () => {
     const onFinish = (value) => {
         console.log(value);
     };
     return (
         <div className='flex justify-center items-center mt-40'>
-            <div className='absolute w-72 sm:w-80 md:w-72 lg:w-80 md:mt-[-460px] md:ml-[393px] lg:right-20 p-6 mt-20 lg:mt-6 bg-white lg:top-64 border border-solid border-[#13C265] mx-auto shadow-lg shadow-bottom-red '>
+            <div className='absolute w-72 sm:w-80 md:w-72 lg:w-80 md:mt-[-460px] md:ml-[393px] lg:right-20 p-6 mt-20 lg:mt-6 bg-white lg:top-64 border border-solid border-[#13C265] mx-auto shadow-lg shadow-bottom mr-4" style="background-color: #13C265; width: 100px; height: 100px;'>
                 <div>
                     <h2 className='lg:text-lg font-semibold '>Get Started today</h2>
                     <p className='lg:text-sm font-light bottom-6'>Start your free trial now and receive free leads for 14 days.</p>
