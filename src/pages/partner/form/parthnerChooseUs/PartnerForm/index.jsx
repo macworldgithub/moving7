e531215a2a13477f12a0fc54acb743dbf3ebfd6a
salman <alysalman15@gmail.com>
2024-02-15 15:38:27 +0500

    freetrail form complete

--- src/pages/partner/form/parthnerChooseUs/PartnerForm/index.jsx
+++ src/pages/partner/form/parthnerChooseUs/PartnerForm/index.jsx
@@ -4,7 +4,7 @@ export default function FreeTrialForm() {
     return (
         <div className='flex items-center justify-center mx-auto'>
             <div>
-                <h2 className='lg:font-semibold text-3xl text-center p-4'>Welcome to free trial</h2>
+                <h2 className='lg:font-semibold text-3xl text-center p-6 mb-4'>Welcome to free trial</h2>
                 <div className=' bg-[#E8FFF3] flex items-center flex-col flex-wrap justify-center rounded-md border-gray-200 border-2 p-3 w-11/12 lg:w-[1140px] mx-auto'>
                     <h2 className='text-[#13C265] text-2xl'>Select removal types</h2>
                     <div className='flex flex-col flex-wrap md:flex-row'>
@@ -38,14 +38,14 @@ export default function FreeTrialForm() {
                             <h2 className='text-[#13C265] text-2xl text-center'>Provide your location and select a radius</h2>
                             <h3 className='text-gray-400 font-extralight text-xl'>Maximum of 20 miles during free trial</h3>
                         </div>
-                        <div className='flex flex-wrap md:gap-4 mt-4'>
+                        <div className='flex flex-wrap gap-2 md:gap-4 mt-4'>
                             <div>
-                                <h3>Your Location</h3>
-                                <input type="text" placeholder='Address' className='w-[14rem] md:w-[18rem] px-1 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
+                                <h3 className='text-[#13C265]'>Your Location</h3>
+                                <input type="text" placeholder='Address' className='w-[14rem] md:w-[18rem] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
                             </div>
                             <div>
-                                <h3>Radius</h3>
-                                <input type="number" placeholder='5 miles' className='w-[12rem] px-1 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
+                                <h3 className='text-[#13C265]'>Radius</h3>
+                                <input type="number" placeholder='5 miles' className=' w-[6rem] md:w-[12rem] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
                             </div>
                         </div>
                     </div>
@@ -53,11 +53,12 @@ export default function FreeTrialForm() {
                         <img src={MapImg} alt="" className=' w-3/4 flex items-center justify-center mx-auto' />
                     </div>
                     <div>
-                        <h2 className='text-[#13C265] text-2xl text-center'>Company details</h2>
+                        <h2 className='text-[#13C265] text-2xl text-center p-4'>Company details</h2>
                         <div>
                             <input type="text" placeholder='Company name' className='w-[255px] md:w-[516px] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
                         </div>
                         <div className='mt-2'>
+                            <h3 className='text-[#13C265]'>Business type</h3>
                             <div className=' bg-white rounded-md border-[#13C26580] border-[1.5px]'>
                                 <div className='flex w-[255px] md:w-[516px] px-2 py-1'>
                                     <input type="radio" />
@@ -71,9 +72,44 @@ export default function FreeTrialForm() {
                                 </div>
                             </div>
                         </div>
-                        <div className='mt-4'>
-                            <h3>Number of employees</h3>
-                            <input type="number" placeholder='Select' className='w-[255px] md:w-[516px] px-1 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
+                        <div className='mt-2 mb-4'>
+                            <h3 className='text-[#13C265]'>Number of employees</h3>
+                            <input type="number" placeholder='Select' className='w-[255px] md:w-[516px] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
+                        </div>
+                        <div>
+                            <h2 className=' text-[#13C265]'>Email</h2>
+                            <input type="text" placeholder='example@gmail.com' className='w-[255px] md:w-[516px] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
+                        </div>
+                        <div>
+                            <h2 className=' text-[#13C265] mt-3'>Telephone</h2>
+                            <input type="text" placeholder='Telephone' className='w-[255px] md:w-[516px] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
+                        </div>
+                        <div>
+                            <h2 className=' text-[#13C265] mt-3'>Address line 1</h2>
+                            <input type="text" placeholder='Address line 1' className='w-[255px] md:w-[516px] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
+                        </div>
+                        <div>
+                            <h2 className=' text-[#13C265] mt-3'>Town/City</h2>
+                            <input type="text" placeholder='Town/City' className='w-[255px] md:w-[516px] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
+                        </div>
+                        <div>
+                            <h2 className=' text-[#13C265] mt-3'>State </h2>
+                            <input type="text" placeholder='Select a State' className='w-[255px] md:w-[516px] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
+                        </div>
+                        <div>
+                            <h2 className='text-[#13C265] text-2xl text-center pt-6'>Contact person</h2>
+                            <div>
+                                <h2 className=' text-[#13C265]'>Salutation</h2>
+                                <input type="text" placeholder='Salutation' className='w-[255px] md:w-[516px] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
+                            </div>
+                            <div>
+                                <h2 className=' text-[#13C265] mt-3'>First name</h2>
+                                <input type="text" placeholder='First name' className='w-[255px] md:w-[516px] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px]' />
+                            </div>
+                            <div>
+                                <h2 className=' text-[#13C265] mt-3'>Last name</h2>
+                                <input type="text" placeholder='Last name' className='w-[255px] md:w-[516px] px-2 py-1 rounded-md border-[#13C26580] border-[1.5px] mb-4' />
+                            </div>
                         </div>
                     </div>
                 </div>
