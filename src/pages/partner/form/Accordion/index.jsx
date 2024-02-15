b65b22128bd14d884a9926963a68d33a09727f8a
salman <alysalman15@gmail.com>
2024-02-15 18:30:40 +0500

    fixed alignments

--- src/pages/partner/form/Accordion/index.jsx
+++ src/pages/partner/form/Accordion/index.jsx
@@ -9,7 +9,7 @@ const RegionAccordion = () => {
         <div className='md:w-[53%] mx-auto'>
             <h2 className='text-lg font-medium mt-4'>Select your areas</h2>
             <p className='text-gray-500'>You can select 5 areas during free trial</p>
-            <Collapse style={{ backgroundColor: "white", borderColor:"#13C26580" }} defaultActiveKey={['1']} onChange={onChange}>
+            <Collapse expandIconPosition="right" style={{backgroundColor: "white", borderColor: "#13C26580", borderBottomColor: "#13C26580", borderWidth: "1.5px" }} defaultActiveKey={['1']} onChange={onChange}>
                 <Panel style={{ fontWeight: "500" }} header="Pakistan" key="1">
                     <div className='flex  flex-wrap gap-x-10 lg:gap-x-60 '>
                         <div className='flex gap-2'>
