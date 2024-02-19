67766c2d475bfdf11181d82a6377f8b8ab1efbf9
salman <alysalman15@gmail.com>
2024-02-19 12:48:35 +0500

    outline added

--- src/pages/moving_type/moving-type.jsx
+++ src/pages/moving_type/moving-type.jsx
@@ -251,11 +251,11 @@ function MovingType() {
             )}
             {inputStates.isVisible_10 && (
               <div className="text-start flex flex-col items-center mt-4">
-                <p className="lg:w-1/2 outline-[#13C265]">
+                <p className="lg:w-1/2">
                   Flat build name/ District apartment/ Office / Villa{" "}
                 </p>
                 <Input
-                  className=" lg:w-2/4"
+                  className=" lg:w-2/4 outline-[#13C265]"
                   onClick={() => handleInputStateChange("isVisible_11", true)}
                   value={data.building}
                   onChange={(e) => handleDataChange("building", e.target.value)}
