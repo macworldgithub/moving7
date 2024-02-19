9a460a60731af32d380d0cb443e188e6e49c2ab8
salman <alysalman15@gmail.com>
2024-02-19 16:38:02 +0500

    added create button

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,6 +1,4 @@
-
 import React from 'react'
-
 import UserImg from '../../assets/images/login/Group.svg'
 import UserIcon from '../../assets/images/login/Vector (1).svg'
 import Lock from '../../assets/images/login/Vector (2).svg'
@@ -9,9 +7,9 @@ import Footer from '../footer/footer'
 export default function Login() {
     return (
         <>
-            <div className=' w-4/5 xl:w-1/3 sm:mt-60 lg:mt-96 bg-[#EDEDED] mx-auto relative rounded-3xl'>
+            <div className=' w-4/5 xl:w-1/3 sm:mt-36 lg:mt-60 bg-[#EDEDED] mx-auto relative rounded-3xl'>
                 <div className='flex items-center justify-center flex-col mt-40 md:mt-0'>
-                    <div className='bg-[#13C265] rounded-full h-[5rem] w-[5rem] md:h-40 md:w-40 flex items-center justify-center p-[1.25rem] md:p-9 -mt-[3rem]  md:-mt-20 mb-12'>
+                    <div className='bg-[#13C265] rounded-full h-[5rem] w-[5rem] md:h-[10rem] md:w-[10rem] lg:h-40 lg:w-40 flex items-center justify-center p-[1.25rem] md:p-9 -mt-[3rem] md:-mt-20 mb-12'>
                         <img src={UserImg} />
                     </div>
                     <div className='flex'>
@@ -26,7 +24,7 @@ export default function Login() {
                         </div>
                         <input type="text" placeholder='Password' className=' md:w-80 pl-3 bg-[#BEDBBE] placeholder:text-white' />
                     </div>
-                    <div className='flex flex-col sm:flex-row gap-2 md:gap-28 mt-11 mb-9'>
+                    <div className='flex flex-col flex-wrap sm:flex-row gap-2 lg:gap-6 mt-11 mb-9'>
                         <div className='flex gap-2'>
                             <input type="checkbox" />
                             <h2 className='font-light'>Remember me</h2>
@@ -34,10 +32,13 @@ export default function Login() {
                         <div>
                             <h2 className='font-light italic'>Forget Password?</h2>
                         </div>
+
+                        <h2 className='italic text-[#13C265] font-medium cursor-pointer'>Create account</h2>
+
                     </div>
                 </div>
             </div>
-            <div className='flex items-center justify-center bg-slate-300 w-44 md:w-96 mx-auto py-4 rounded-b-3xl bg-gradient-to-b from-[#13C265] to-[#BEDBBE] text-white md:text-2xl mb-20 lg:mb-96'>
+            <div className='flex items-center justify-center bg-slate-300 w-44 md:w-96 mx-auto py-4 rounded-b-3xl bg-gradient-to-b from-[#13C265] to-[#BEDBBE] text-white md:text-2xl mb-20 lg:mb-20'>
                 <button>Login</button>
             </div>
             <Footer />
