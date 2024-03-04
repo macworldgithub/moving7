4d77a5e7cb06ef5106eb07c8d0f04f08f10d7507
salman <alysalman15@gmail.com>
2024-03-04 18:11:13 +0500

    Add navigation for mobile Header

--- src/pages/header/becomePartner-Header/forMobile/index.jsx
+++ src/pages/header/becomePartner-Header/forMobile/index.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
+import { Link } from "react-router-dom";
+
 
 export default function MobileMenu() {
     return (
         <div className='w-full lg:w-72 flex flex-col flex-wrap items-center justify-center text-sm sm:text-md md:text-lg mt-[2.5rem] font-medium border-b-2 gap-1 px-6 py-8 right-8 z-50 text-white bg-[#1ae073] rounded-md lg:hidden'>
-            <h2>Overview</h2>
-            <h2>Quote Requests</h2>
-            <h2>Company Profile</h2>
-            <h2>Targeting</h2>
-            <h2>Help Desk</h2>
+            <Link to="/overview">
+                <h2>Overview</h2>
+            </Link>
+            <Link to="/quoteRequest">
+                <h2>Quote Requests</h2>
+            </Link>
+            <Link to="/">
+                <h2>Company Profile</h2>
+            </Link>
+            <Link to="/targeting">
+                <h2>Targeting</h2>
+            </Link>
+            <Link to="/">
+                <h2>Help Desk</h2>
+            </Link>
             <div className='flex items-center gap-2'>
-                <h2>Account</h2>
+                <Link to="/account">
+                    <h2>Account</h2>
+                </Link>
                 <IoIosArrowDown />
             </div>
         </div>
