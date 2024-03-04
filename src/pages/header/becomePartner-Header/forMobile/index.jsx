1c6d142559ff04100853e2acdb576ac611193107
salman <alysalman15@gmail.com>
2024-03-04 17:38:33 +0500

    fixed alignment for mobile menu

--- src/pages/header/becomePartner-Header/forMobile/index.jsx
+++ src/pages/header/becomePartner-Header/forMobile/index.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { IoIosArrowDown } from "react-icons/io";
-
+import React from 'react';
+import { IoIosArrowDown } from 'react-icons/io';
 
 export default function MobileMenu() {
     return (
-        <div className='w-72 flex flex-col flex-wrap items-center justify-center text-lg mt-[2.5rem] font-medium border-b-2 gap-1 px-6 py-8 right-8 z-50 text-white bg-[#1ae073] rounded-md lg:hidden'>
+        <div className='w-full lg:w-72 flex flex-col flex-wrap items-center justify-center text-lg mt-[2.5rem] font-medium border-b-2 gap-1 px-6 py-8 right-8 z-50 text-white bg-[#1ae073] rounded-md lg:hidden'>
             <h2>Overview</h2>
             <h2>Quote Requests</h2>
             <h2>Company Profile</h2>
@@ -15,5 +14,5 @@ export default function MobileMenu() {
                 <IoIosArrowDown />
             </div>
         </div>
-    )
+    );
 }
