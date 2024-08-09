0c71bddeb1af4b69f0b1ef0dad3037451a4c9492
salman <alysalman15@gmail.com>
2024-08-09 18:06:18 +0500

    fixed alignment

--- src/pages/partner/form/Accordion/index.jsx
+++ src/pages/partner/form/Accordion/index.jsx
@@ -2,51 +2,46 @@ import { Collapse } from 'antd';
 import React from 'react';
 const { Panel } = Collapse;
 
-let arr = [{ name: "Lahore" }, { name: "Karachi" }, { name: "Sialkot" }]
+let arr = [{ name: "Lahore" }, { name: "Karachi" }, { name: "Sialkot" }];
 
-const RegionAccordion = ({name, areas,fetchPolygon, setData, data }) => {
+const RegionAccordion = ({ name, areas, fetchPolygon, setData, data }) => {
 
-    
-    console.log(data, "accc");
-    let renderData = areas ?? arr
+    console.log(areas, "accc");
+    let renderData = areas ?? arr;
+
     return (
         <div className='md:w-[52%] w-[90%] my-1 mx-auto'>
-            <Collapse expandIconPosition="right" style={{ backgroundColor: "white", borderColor: "#13C26580", borderBottomColor: "#13C26580", borderWidth: "1.5px" }} >
+            <Collapse
+                expandIconPosition="right"
+                style={{ backgroundColor: "white", borderColor: "#13C26580", borderBottomColor: "#13C26580", borderWidth: "1.5px" }}
+            >
                 <Panel style={{ fontWeight: "500" }} header={name} key="1">
-                    <div className='flex  flex-wrap gap-x-10 lg:gap-x-60 '>
+                    <div className='flex flex-wrap gap-x-10 justify-between'>
+
                         {
                             renderData?.map((city, i) => {
                                 return (
-                                    <div className='flex gap-2' key={i}>
-                                        <input type="checkbox"
+                                    <div className='flex items-center gap-2 w-2/5' key={i}>
+                                        <input
+                                            type="checkbox"
                                             checked={data?.regions?.includes(city)}
                                             onChange={(e) => {
-                                                if (e.target.checked === true) {
-
-                                                    let temp = data
-                                                    if (!temp?.regions){
-                                                        temp.regions = []
-                                                    }
-
-                                                    temp?.regions?.push(city)
-                                                    setData({
-                                                        ...data,
-                                                        regions: temp?.regions
-                                                    })
+                                                let temp = { ...data };
+                                                if (e.target.checked) {
+                                                    if (!temp?.regions) temp.regions = [];
+                                                    temp.regions.push(city);
                                                 } else {
-                                                    let temp = data
-                                                    temp?.regions?.splice(temp?.regions?.findIndex(region => region.name === city.name), 1);
-                                                    setData({
-                                                        ...data,
-                                                        regions: temp.regions
-                                                    })
+                                                    temp.regions.splice(temp.regions.findIndex(region => region.name === city.name), 1);
                                                 }
-                                                fetchPolygon()
-
-                                            }} className='accent-[#13C265]' style={{ width: "14px" }} />
+                                                setData(temp);
+                                                fetchPolygon();
+                                            }}
+                                            className='accent-[#13C265]'
+                                            style={{ width: "14px" }}
+                                        />
                                         <p className='text-md'>{city.name}</p>
                                     </div>
-                                )
+                                );
                             })
                         }
                     </div>
@@ -55,4 +50,5 @@ const RegionAccordion = ({name, areas,fetchPolygon, setData, data }) => {
         </div>
     );
 };
+
 export default RegionAccordion;
